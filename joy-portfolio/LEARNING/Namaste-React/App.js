4998c10c2fd4b6604f77a7e3994e6812f7932503
joysanks <saikiajoysankar@gmail.com>
2026-06-06 12:04:25 +0530

    NR e2

--- joy-portfolio/LEARNING/Namaste-React/App.js
+++ joy-portfolio/LEARNING/Namaste-React/App.js
@@ -7,23 +7,40 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
-
 // ======================Next part=====================
 // nested
+// import React from "react";
+// import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    React.createElement(
-        "div",
-        {id: "child"},
-        [React.createElement("h1", {}, "I am h1 tag"),
-        React.createElement("h2", {}, "I am h2 tag"),
-        ]// array of children
-    )
-);
+// const parent = React.createElement(
+//     "div",
+//     {id: "parent"},
+//     React.createElement(
+//         "div",
+//         {id: "child"},
+//         [React.createElement("h1", {}, "I am h1 tag"),
+//         React.createElement("h2", {}, "I am h2 tag"),
+//         ]// array of children
+//     )
+// );
 
-console.log(parent); // is a object which is called React Element. It is a lightweight object which is used to create the virtual DOM.
+// console.log(parent); // is a object which is called React Element. It is a lightweight object which is used to create the virtual DOM.
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
+
+// ======================Next part=====================
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// React Element
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+
+console.log(jsxHeading); // is a object which is called React Element. It is a lightweight object which is used to create the virtual DOM.
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
