688887536e684943a51a928009541ceb2a2c8e3a
joysanks <saikiajoysankar@gmail.com>
2026-06-16 20:47:07 +0530

    NR E 6 7

--- joy-portfolio/LEARNING/Namaste-React/src/components/Body.js
+++ joy-portfolio/LEARNING/Namaste-React/src/components/Body.js
@@ -1,13 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import RestaurantCard from "./RestaurentCard";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurents, setListOfRestaurents] = useState(resList);
+console.log("listOfRestaurents",listOfRestaurents);
+const [filteredRestaurent, setFilteredRestaurent]  = useState(resList);
+
+  const [searchText, setSearchText] = useState("");
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
+
+  // const fetchData = async () => {
+  //   const data = await fetch("https://namastedev.com/api/v1/listRestaurants"
+  //   );
+
+  //   const json = await data.json;
+  //   console.log(json);
+  //   // setListOfRestaurents(json?.data?.cards[2]?.data?.data?.cards);
+  // };
+
+  if (listOfRestaurents.length === 0) {
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">                                                                                                                                                                                                                                                                                                                                    
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          ></input>
+          <button
+            onClick={() => {
+              console.log(searchText);
+                const filteredRestaurents = listOfRestaurents.filter((res) => 
+                  res.info.name.toLowerCase().includes(searchText.toLowerCase())
+                );
+
+                setFilteredRestaurent(filteredRestaurents);
+            }}
+          >
+            search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -22,7 +65,7 @@ const Body = () => {
       </div>
 
       <div className="res-container">
-        {listOfRestaurents.map((restaurent) => (
+        {filteredRestaurent.map((restaurent) => (
           <RestaurantCard key={restaurent.info.id} resData={restaurent} />
         ))}
       </div>
