4326a8b1588aa94f228cd2f41b43d58d6b04e64d
joysanks <saikiajoysankar@gmail.com>
2026-07-12 00:43:05 +0530

    NR E-10 tailwind 3

--- joy-portfolio/LEARNING/Namaste-React/src/components/RestaurentCard.js
+++ joy-portfolio/LEARNING/Namaste-React/src/components/RestaurentCard.js
@@ -14,16 +14,16 @@ const RestaurantCard = (props) => {
   } = resData.info;
 
   return (
-    <div className="m-4 p-4 w-[251px]" style={{ backgroundColor: "#f0f0f0" }}>
+    <div className="m-4 p-4 w-[251px] rounded-lg bg-gray-100 hover:bg-gray-200" >
       <img
-        className="res-logo"
+        className="rounded-lg"
         src={
           CDN_URL +
           cloudinaryImageId
         }
         alt="res-logo"
       ></img>
-      <h3>{name}</h3>
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{`${avgRating} by ${totalRatingsString} users`}</h4>
       <h4>{costForTwo}</h4>
